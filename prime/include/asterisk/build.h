445b3648b9e3bf94a5d2dd791f241eb2b0a1634c
8none1 <github@whizzy.org>
2016-11-21 16:39:07 +0000

    first commit

---
+++ prime/include/asterisk/build.h
@@ -0,0 +1,11 @@
+/*
+ * build.h 
+ * Automatically generated
+ */
+#define BUILD_HOSTNAME "malfunctioning-eddie"
+#define BUILD_KERNEL "4.4.0-38-generic"
+#define BUILD_MACHINE "x86_64"
+#define BUILD_OS "Linux"
+#define BUILD_DATE "2016-10-14 11:48:27 UTC"
+#define BUILD_USER "will"
+
